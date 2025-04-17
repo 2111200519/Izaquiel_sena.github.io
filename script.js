069e170833b7940f8e4945fccee65dc0010106af
2111200519 <izaquielpereiradesena2@gmail.com>
2025-04-16 21:19:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let display = document.getElementById("display");
+}
 function adicionarValor(valor) {
     display.value += valor;
+}
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
 }
